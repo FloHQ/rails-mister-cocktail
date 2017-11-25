a38767e7631f19a90c074efb837c09e58c823842
Florian Herlicq <fherlicq@gmail.com>
2017-11-25 15:22:34 +0100

    fixed minor bugs

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = Figaro.env.CLOUDINARY_CLOUD_NAME
+  config.api_key = Figaro.env.CLOUDINARY_API_KEY
+  config.api_secret = Figaro.env.CLOUDINARY_API_SECRET
+  config.cdn_subdomain = true
+end
